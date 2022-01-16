ac7788f88a52f8041c9319ba5c5c33fead973a13
sivajiyelugoti <sivaji.y@gmail.com>
2022-01-16 01:28:49 +0000

    added lombok

--- src/main/java/com/java8/exercise/classes/MethodReferencesInJava.java
+++ src/main/java/com/java8/exercise/classes/MethodReferencesInJava.java
@@ -1,10 +1,15 @@
 package com.java8.exercise.classes;
 
+import com.sun.xml.internal.ws.util.StringUtils;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class MethodReferencesInJava {
     public static void main(String[] args) {
         List<String> messages = Arrays.asList("hello", "baeldung", "readers!");
+        messages.forEach(word-> StringUtils.capitalize(word));
+        messages.forEach(StringUtils::capitalize);
+        System.out.println(messages);
     }
 }
